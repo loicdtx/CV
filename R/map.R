9b04045bb21ebc2b22d8f4c8c6b8407ea17eb071
Loïc <loic.dutrieux@gmail.com>
2015-10-23 18:17:36 +0200

    Added map of places where I've lived

---
+++ R/map.R
@@ -0,0 +1,48 @@
+library(ggplot2)
+library(raster)
+library(sp)
+library(rgeos)
+library(magrittr)
+
+world <- getData(name = 'countries')
+str(world@data)
+levels(world@data$UNREG1)
+# worldSub <- world[world$UNREG1 %in% c("Western Europe"),]
+worldSub <- world[world$ENGLISH %in% c('France', 'Belgium', 'Netherlands', 'Luxembourg'),]
+plot(worldSub)
+
+
+FRA <- getData(name = 'GADM', country = 'FRA', level = 2)
+FRA <- FRA[FRA$NAME_2 %in% c('Ardèche', 'Alpes-de-Haute-Provence', 'Maine-et-Loire'),]
+FRA <- gCentroid(FRA, byid = TRUE)
+
+NL <- getData(name = 'GADM', country = 'NLD', level = 1)
+NL <- NL[NL$NAME_1 %in% c('Gelderland'),]
+NL <- gCentroid(NL, byid = TRUE)
+
+places <- rbind(NL, FRA) %>%
+  spTransform(CRS('+proj=robin')) %>%
+  data.frame()
+
+ggEurope <- spTransform(worldSub, CRS('+proj=robin')) %>%
+  fortify(worldSub, region = 'NAME')
+
+ggplot(ggEurope) +
+  geom_polygon(aes(x=long, y=lat, group=group)) +
+  geom_polygon(aes(x=long, y=lat, group=group), colour = 'white', size = 0.2, fill = NA) +
+  geom_point(data = places, aes(x, y), color = 'red', size = 3) +
+  coord_equal() +
+  theme_bw() +
+  theme(axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.background=element_rect(fill = "transparent",colour = NA),
+        panel.border=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        plot.background=element_rect(fill = "transparent",colour = NA))
+
+ggsave(filename = 'img/map.png', width = 4, height = 4, bg = 'transparent')
