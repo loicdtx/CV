5e2cfdd27c15ddd793bc6a1c4bf00cee5683f21c
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-02-28 14:30:47 -0600

    Basic translation of CV in spanish

--- build_for_website.sh
+++ build_for_website.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 xelatex -output-directory=/home/loic/Dropbox/cv/ cv.tex
-mv ~/Dropbox/cv/cv.pdf ~/Dropbox/cv/loic_dutrieux_cv_en.pdf
+mv ~/Dropbox/cv/cv.pdf ~/Dropbox/cv/loic_dutrieux_cv_es.pdf
 
